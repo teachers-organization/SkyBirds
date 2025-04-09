a09a4685e236c9b003043aa237c80b9ac655cd59
Aitana Juan <aitanajh7@gmail.com>
2025-04-09 11:10:42 +0200

    Crear página de home

--- app/src/main/java/com/example/skybird/View/HomeView.kt
+++ app/src/main/java/com/example/skybird/View/HomeView.kt
@@ -1,20 +1,127 @@
 package com.example.skybird.View
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.skybird.R
 
 @Composable
-fun Home(){
+fun Home() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        //Menú superior
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color(0xFFADD8E6))
+                .padding(top = 40.dp, start = 20.dp, end = 20.dp, bottom = 20.dp)
+                .align(Alignment.TopCenter),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.user),
+                contentDescription = "User Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .height(60.dp)
+                    .width(60.dp)
+                    .clip(CircleShape)
+            )
 
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.White))
-    {
+            Text(
+                text = "Aitana",
+                fontSize = 25.sp,
+                color = Color.Black,
+                modifier = Modifier.padding(start = 8.dp)
+            )
 
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF5A7391),
+                    contentColor = Color.White
+                ),
+                modifier = Modifier
+                    .height(60.dp)
+                    .width(60.dp)
+            ) { }
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(40.dp)
+        ) {
+            Button(
+                onClick = { /* Acción 1 */ },
+                shape = RoundedCornerShape(50),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF5A7391),
+                    contentColor = Color.White
+                ),
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .height(50.dp)
+            ) {
+                Text("Diccionario de aves")
+            }
+
+            Button(
+                onClick = { /* Acción 2 */ },
+                shape = RoundedCornerShape(50),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF5A7391),
+                    contentColor = Color.White
+                ),
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .height(50.dp)
+            ) {
+                Text("Foro")
+            }
+
+            Button(
+                onClick = { /* Acción 3 */ },
+                shape = RoundedCornerShape(50),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF5A7391),
+                    contentColor = Color.White
+                ),
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .height(50.dp)
+            ) {
+                Text("Avistamientos")
+            }
+        }
     }
-
 }
